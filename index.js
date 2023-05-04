3bfbc7cc8515839ae8df258761321332c1695938
Nils Matic <matic1909@gmail.com>
2023-05-04 18:46:28 +0200

    added functions for each basic operation and operate function

---
+++ index.js
@@ -0,0 +1,26 @@
+let firstNumber = null;
+let secondNumber = null;
+let operation = null;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  if (operator === '+') return add(num1, num2);
+  if (operator === '-') return subtract(num1, num2);
+  if (operator === '*') return multiply(num1, num2);
+  if (operator === '/') return divide(num1, num2);
+}
