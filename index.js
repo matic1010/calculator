53514a2f71bffdee948656d9398919720fad148c
Nils Matic <matic1909@gmail.com>
2023-11-20 16:41:34 +0100

    added calculator buttons and event listeners for numbers and operators

--- index.js
+++ index.js
@@ -13,3 +13,52 @@ const operate = (op, a, b) => {
   if(op === "*") return multiply(a, b);
   if(op === "/") return divide(a, b);
 }
+
+const opButtons = document.querySelectorAll(".op-button")
+
+opButtons.forEach(button => {
+  const op = button.dataset.operator
+  button.addEventListener(("click"), () => {
+    // if calculation has not been operated yet, do that
+    if(operator) {
+      const result = operate(operator, firstNumber, secondNumber)
+      console.log(result)
+      firstNumber = result;
+      secondNumber = null;
+      operator = null;
+    }
+    // set the operator
+    operator = op
+  })
+})
+
+const numberButtons = document.querySelectorAll(".number-button")
+
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    if(!operator) {
+      if(!firstNumber) {
+        firstNumber = Number(button.dataset.value)
+      } else {
+        firstNumber = Number(firstNumber += button.dataset.value)
+      }
+    } else {
+      if(!secondNumber) {
+        secondNumber = Number(button.dataset.value)
+      } else {
+        secondNumber = Number(secondNumber += button.dataset.value)
+      }
+    }
+  })
+})
+
+const equalsButton = document.querySelector("#equals")
+equalsButton.addEventListener("click", () => {
+  if(firstNumber && secondNumber && operator) {
+    const result = operate(operator, firstNumber, secondNumber)
+    console.log(result)
+    firstNumber = result;
+    secondNumber = null;
+    operator = null;
+  }
+})
